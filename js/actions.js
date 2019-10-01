b9292de4d76cec6734ebedabee87f93815522d32
Artūras <murashka.arturas@gmail.com>
2019-10-01 14:11:47 +0300

    Services section #32

--- js/actions.js
+++ js/actions.js
@@ -3,6 +3,7 @@ var t0 = performance.now();
 renderNavigation( navigation_links, 'nav' );
 renderNavigation( navigation_links, '.mobile-nav' );
 navigationFunctionality();
+renderServices ( services );
 
 var t1 = performance.now();
 console.log("" + (t1 - t0) + "ms");
